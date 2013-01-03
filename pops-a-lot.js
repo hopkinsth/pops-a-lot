d30bfa28ae3c4e769de124f08fc2255313bcfb01
thopkins <thopkins@redventures.com>
2013-01-03 14:28:27 -0500

    added dividers for the menu, finally

---
+++ pops-a-lot.js
@@ -0,0 +1,16 @@
+(function (jq) {
+	jq(function () {
+		//adds the separator elements to the menu, which, unfortunately,
+		//need to be separate elements
+		jq('#the-menu .main-menu > li').not(':last-child').after('<li class="separator"/>');
+
+		jq('.main-menu li').addClass(function () {
+			//if the element has sub ul elements, 
+			//add a class to it so we can distinguish it from those that don't
+			console.log(this);
+			if (jq(this).children('ul').length > 0) {
+				return 'has-children';
+			} 
+		});
+	});
+}(window.jQuery));
