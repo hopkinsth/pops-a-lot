b7bbf6be58adc8193f4155f1ada9a27a672fa675
thopkins <thopkins@redventures.com>
2013-01-20 01:41:40 -0500

    bunch o' changes

--- pops-a-lot.js
+++ pops-a-lot.js
@@ -1,8 +1,31 @@
 (function (jq, sw) {
 	jq(function () {
-		var topLevelListItems = jq('.main-menu > li');
+		var topLevelListItems = jq('.main-menu > li'),
+			contentArea = jq('#content_area'),
+			theBody = jq('#the-body'),
+			flavorNavHTML,
+			prependFlavorNav;
+
+		flavorNavHTML = '<div class="clear"></div><div id="flavor-nav"><div class="nav-item london-kettle"><a href="/Articles.asp?ID=260"></a></div><div class="nav-item clandestine-caramel"><a href="/Articles.asp?ID=269"></a></div><div class="nav-item roman-holiday"><a href="/Articles.asp?ID=267"></a></div><div class="nav-item saigon-sunrise"><a href="/Articles.asp?ID=268"></a></div><div class="nav-item brazilian-samba last"><a href="/Articles.asp?ID=261"></a></div><div class="nav-item southern-surprise"><a href="/Articles.asp?ID=263"></a></div><div class="nav-item honolulu-aloha"><a href="/Articles.asp?ID=262"></a></div></div><div class="clear"></div>';
 		//adds the separator elements to the menu, which, unfortunately,
 		//need to be separate elements
+
+		//if there's a div in the content with the id copy-to-external, grab
+		//the elements in the contents div and move them elsewhere
+		//to thwart volusion's stupid template "system"
+		
+		if (jq('#content_area #copy-to-external').length === 1) {
+			if (contentArea.find('#prepend-flavor-menu').length === 1) {
+				prependFlavorNav = true;
+			}
+
+			theBody.prepend( contentArea.find('#to-copy').html() );
+			contentArea.remove();
+
+			if (prependFlavorNav) {
+				theBody.prepend( flavorNavHTML );
+			}
+		}
 
 		topLevelListItems.each(function () {
 			//adds item<num> classes to the top-level items
